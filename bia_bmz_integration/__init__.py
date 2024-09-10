4fcd7cd2d1219c25e38a117796c2e6f0087ef374
Craig Russell <ctr26@ebi.ac.uk>
2024-09-10 13:08:19 +0100

    [fix] processing is working now

--- bia_bmz_integration/__init__.py
+++ bia_bmz_integration/__init__.py
@@ -106,16 +106,23 @@ def reorder_array_dimensions(in_tensor, in_id):
 
 
 # Function to handle model inference
-def run_model_inference(bmz_model, arr):
+def run_model_inference(bmz_model, image):
     # load model
     model_resource = bioimageio.core.load_description(bmz_model)
-
+    expected_input_shape = model_resource.inputs[0].shape
+    
+    # try:
     test_input_image = load_array(model_resource.inputs[0].test_tensor)
+    # except Exception as e:
+    #     test_input_image = load_array(model_resource.test_inputs[0])
+    # if image.shape != expected_input_shape:
+    #     raise ValueError(f"Input image shape {image.shape} does not match expected shape {expected_input_shape}")
+    
     if isinstance(model_resource, v0_5.ModelDescr):
         # Reshape data to match model input dimensions
         # new_np_array = np.squeeze(arr).compute()
         indices = [i for i in range(len(test_input_image.shape)) if test_input_image.shape[i] == 1]
-        right_dims = np.expand_dims(arr, indices)
+        right_dims = np.expand_dims(image, indices)
         input_tensor = Tensor.from_numpy(right_dims, dims=tuple(model_resource.inputs[0].axes))
 
         # Create collection of tensors (sample)
@@ -124,7 +131,7 @@ def run_model_inference(bmz_model, arr):
         sample = Sample(members={inp_id: input_tensor}, stat={}, id="id")
 
     elif isinstance(model_resource, v0_4.ModelDescr):
-        new_np_array = np.squeeze(arr).compute()
+        new_np_array = np.squeeze().compute()
         indices = [i for i in range(len(test_input_image.shape)) if test_input_image.shape[i] == 1]
         right_dims = np.expand_dims(new_np_array, indices)
         input_tensor = Tensor.from_numpy(right_dims, dims=tuple(model_resource.inputs[0].axes))
@@ -140,7 +147,7 @@ def run_model_inference(bmz_model, arr):
     prediction_pipeline = create_prediction_pipeline(model_resource)
     prediction = prediction_pipeline.predict_sample_without_blocking(sample)
 
-    return prediction, sample, inp_id, outp_id
+    return prediction, sample, inp_id, outp_id, test_input_image
 
 
 # Function to benchmark results
@@ -206,45 +213,173 @@ def process(bmz_model,
             channel=None, 
             t_slices=None, 
             benchmark_channel=0):
-    # Load image and annotations
+    # load image
     dask_array = remote_zarr_to_model_input(ome_zarr_uri)
+
+    # load reference annotations
     ref_array = remote_zarr_to_model_input(reference_annotations)
 
-    # Optional cropping
+    # crop image if needed
     if crop_image:
-        dask_array = crop_center(dask_array, crop_image)
-        ref_array = crop_center(ref_array, crop_image)
-
-    # Slices and channel selection
+        dask_array=crop_center(dask_array,crop_image)
+        ref_array=crop_center(ref_array,crop_image)
+    # select planes and channels of interest
     if z_slices:
-        dask_array = dask_array[:, :, z_slices[0]:z_slices[1], :, :]
-        ref_array = ref_array[:, :, z_slices[0]:z_slices[1], :, :]
+        dask_array = dask_array[:,:,z_slices[0]:z_slices[1],:,:]
+        ref_array = ref_array[:,:,z_slices[0]:z_slices[1],:,:]
+        if dask_array.ndim < 5:
+            dask_array = np.expand_dims(dask_array, 2)
+            ref_array = np.expand_dims(ref_array, 2)
     if channel is not None:
-        dask_array = dask_array[:, channel, :, :, :]
-        ref_array = ref_array[:, channel, :, :, :]
+        dask_array = dask_array[:,channel,:,:,:]
+        dask_array = np.expand_dims(dask_array, 1)
+        ref_array = ref_array[:,channel,:,:,:]
+        ref_array = np.expand_dims(ref_array, 1)
     if t_slices:
-        dask_array = dask_array[t_slices[0]:t_slices[1], :, :, :, :]
-        ref_array = ref_array[t_slices[0]:t_slices[1], :, :, :, :]
-
-    # Run model inference
-    new_np_array = np.squeeze(dask_array).compute()
-    prediction, sample, inp_id, outp_id = run_model_inference(bmz_model, new_np_array, np.array(ref_array))
-
-    # Process results for benchmarking
-    output_array = reorder_array_dimensions(prediction, outp_id)
-    input_array = reorder_array_dimensions(sample, inp_id)
-    correct_ch_output = np.take(output_array, [benchmark_channel], 1)
+        dask_array = dask_array[t_slices[0]:t_slices[1],:,:,:,:]
+        ref_array = ref_array[t_slices[0]:t_slices[1],:,:,:,:]
     
-    # Binarize output and reference arrays
-    binary_output = correct_ch_output >= 0.5
+    # load model
+    model_resource = bioimageio.core.load_description(bmz_model)
+
+    # test model
+
+    # test_summary = test_model(model_resource)
+    # print(test_summary)
+
+    # check model specification version
+    if isinstance(model_resource, v0_5.ModelDescr):
+        # load test image
+        test_input_image = load_array(model_resource.inputs[0].test_tensor)
+
+        # match test data type with the data type of the model input
+        dask_array = dask_array.astype(test_input_image.dtype)
+
+        # reshape data to match model input dimensions
+        new_np_array=np.squeeze(dask_array).compute()
+
+        indices = [i for i in range(len(test_input_image.shape)) if test_input_image.shape[i] == 1]
+        right_dims = np.expand_dims(new_np_array, indices)
+
+        # convert image to tensor
+        input_tensor = Tensor.from_numpy(right_dims, dims=tuple(model_resource.inputs[0].axes))
+
+        # create collection of tensors (sample)
+        inp_id=model_resource.inputs[0].id
+        outp_id=model_resource.outputs[0].id
+        sample = Sample(members={inp_id: input_tensor}, stat={}, id="id")
+
+    elif isinstance(model_resource, v0_4.ModelDescr):
+        # load test image
+        test_input_image = load_array(model_resource.test_inputs[0])
+
+        # match test data type with the data type of the model input
+        dask_array = dask_array.astype(test_input_image.dtype)
+
+        # reshape data to match model input dimensions
+        new_np_array=np.squeeze(dask_array).compute()
+
+        indices = [i for i in range(len(test_input_image.shape)) if test_input_image.shape[i] == 1]
+        right_dims = np.expand_dims(new_np_array, indices)
+
+        # convert image to tensor 
+        input_tensor = Tensor.from_numpy(right_dims, dims=tuple(model_resource.inputs[0].axes))
+
+        # create collection of tensors (sample)
+        inp_id=model_resource.inputs[0].name
+        outp_id=model_resource.outputs[0].name
+        sample = Sample(members={inp_id: input_tensor}, stat={}, id="id")
+
+    else:
+        print("This model specification version is not supported")
+
+    # create prediction pipeline
+
+    devices = None
+    weight_format = None
+
+    prediction_pipeline = create_prediction_pipeline(
+        model_resource, devices=devices, weight_format=weight_format
+    )
+
+    # run prediction
+
+    prediction: Sample = prediction_pipeline.predict_sample_without_blocking(sample)
+
+    # reorder prediction dimensions to "TCZYX" so metrics can be computed 
+    output_array = reorder_array_dimensions(prediction,outp_id)
+    input_array = reorder_array_dimensions(sample,inp_id)
+
+    # select correct channel to benchmark from prediction
+    correct_ch_output=np.take(output_array, [benchmark_channel],1)
+
+    # create prediction torch tensor and binarize prediction to calculate metrics 
+    t_output = torch.from_numpy(correct_ch_output)
+    binary_output = correct_ch_output >= 0.5 #0.95
     t_binary_output = torch.from_numpy(binary_output)
+    t_input = torch.from_numpy(input_array)
+
+    # create reference torch tensor and binarize reference annotation 
+    ref_array = np.asarray(ref_array)
+    ref_array = ref_array.astype(test_input_image.dtype)
     t_reference = torch.from_numpy(ref_array)
-    t_reference_binary = torch.from_numpy(ref_array.astype(bool))
-    max_val = np.max(ref_array) - np.min(ref_array)
+    (t_reference.shape)
+    ref_array_binary = ref_array.astype(bool)
+    t_reference_binary = torch.from_numpy(ref_array_binary)
 
-    # Run benchmarks
-    benchmark_results(t_binary_output, t_binary_output, input_array, t_reference, t_reference_binary, max_val)
+    # compute benchamarking metrics
 
-    # Plot images
+    # segmentation
+    iou_metric = MeanIoU(include_background=True, reduction="mean")
+    iou_score=iou_metric(y_pred=t_binary_output, y=t_reference_binary)
+    print(f"IoU score: {iou_score}")
+
+    dice_metric = DiceMetric(include_background=True, reduction="mean")
+    dice_score = dice_metric(y_pred=t_binary_output, y=t_reference_binary)
+    print(f"Dice score: {dice_score}")
+    
+    # reconstruction
+    max_val = np.max(ref_array)-np.min(ref_array)
+    
+    psnr_metric = PSNRMetric(max_val, reduction="mean", get_not_nans=False)
+    psnr_score = psnr_metric(y_pred=t_output, y=t_reference)
+    print(f"PSNR score reconstructed image: {psnr_score}")
+    psnr_score = psnr_metric(y_pred=t_input, y=t_reference)
+    print(f"PSNR score input image: {psnr_score}")
+    
+    rmse_metric = RMSEMetric(reduction="mean", get_not_nans=False)
+    rmse_score = rmse_metric(y_pred=t_output, y=t_reference)
+    print(f"RMSE score reconstructed image: {rmse_score}")
+    rmse_score = rmse_metric(y_pred=t_input, y=t_reference)
+    print(f"RMSE score input image: {rmse_score}")
+
+    ssim_metric = SSIMMetric(spatial_dims = 2, data_range=max_val)
+    ssim_score = ssim_metric(y_pred=t_output[0,:,:,:,:], y=t_reference[0,:,:,:,:])
+    print(f"SSIM score reconstructed image: {ssim_score}")
+    ssim_score = ssim_metric(y_pred=t_input[0,:,:,:,:], y=t_reference[0,:,:,:,:])
+    print(f"SSIM score input image: {ssim_score}")
+
+
     if plot_images:
-        plot_images(dask_array, output_array, ref_array, binary_output, t_reference_binary, benchmark_channel)
+        plt.figure()
+        ax1 = plt.subplot(2, 3, 1)
+        ax1.set_title("Input")
+        ax1.axis("off")
+        plt.imshow(dask_array[0,0,0,:,:])
+        ax2 = plt.subplot(2, 3, 2)
+        ax2.set_title("Prediction")
+        ax2.axis("off")
+        plt.imshow(correct_ch_output[0,0,0,:,:])
+        ax3 = plt.subplot(2, 3, 3)
+        ax3.set_title("Reference annotation")
+        ax3.axis("off")
+        plt.imshow(ref_array[0,0,0,:,:])
+        ax4 = plt.subplot(2, 3, 4)
+        ax4.set_title("Reference annotation binary")
+        ax4.axis("off")
+        plt.imshow(ref_array_binary[0,0,0,:,:])
+        ax5 = plt.subplot(2, 3, 5)
+        ax5.set_title("Prediction binary")
+        ax5.axis("off")
+        plt.imshow(binary_output[0,0,0,:,:])
+        plt.show()
--- bia_bmz_integration/tests/test_process.py
+++ bia_bmz_integration/tests/test_process.py
@@ -2,44 +2,50 @@ import pytest
 import numpy as np
 from bia_bmz_integration import process,run_model_inference
 
-tensors = [
-    np.random.rand(3,512,512),
-    np.random.rand(1,512,512),
-    np.random.rand(3,1024,1024),
-    np.random.rand(1,1024,1024),
+# tensors = [
+#     np.random.rand(3,512,512),
+#     np.random.rand(1,512,512),
+#     np.random.rand(3,1024,1024),
+#     np.random.rand(1,1024,1024),
 
-]
+# ]
 
 models = ["loyal-squid", "noisy-fish", "affable-shark", "powerful-chipmunk", "loyal-parrot", "faithful-chicken", "frank-water-buffalo"]
-datasets = ["S-BIAD1026", "S-BIAD634", "S-BIAD144/S-BIAD916", "BSST666"]
-datasets = ["https://uk1s3.embassy.ebi.ac.uk/bia-zarr-test/23_low.ome.zarr/0",
-            "https://uk1s3.embassy.ebi.ac.uk/bia-zarr-test/23_high.ome.zarr/0",
-            "https://uk1s3.embassy.ebi.ac.uk/bia-zarr-test/experimentA_01_WT_PDMP.ome.zarr/0",
-            "https://uk1s3.embassy.ebi.ac.uk/bia-zarr-test/normal_26.ome.zarr/0",
-            "https://uk1s3.embassy.ebi.ac.uk/bia-zarr-test/1135_n_H2BtdTomato.ome.zarr/0",
-            "https://uk1s3.embassy.ebi.ac.uk/bia-zarr-test/1135_n_stain_StarDist_goldGT_180_rotation.ome.zarr/0",
-            ]
+# datasets = ["S-BIAD1026", "S-BIAD634", "S-BIAD144/S-BIAD916", "BSST666"]
 
-@pytest.mark.parametrize("bmz_model", models)
+datasets = [
+("frank-water-buffalo","https://uk1s3.embassy.ebi.ac.uk/bia-zarr-test/23_low.ome.zarr/0","https://uk1s3.embassy.ebi.ac.uk/bia-zarr-test/23_high.ome.zarr/0"),
+
+("powerful-chipmunk", "https://uk1s3.embassy.ebi.ac.uk/bia-zarr-test/experimentA_01_WT_PDMP.ome.zarr/0", "https://uk1s3.embassy.ebi.ac.uk/bia-zarr-test/experimentA_01_WT_PDMP_cp_mask.ome.zarr/0" ),
+
+("affable-shark","https://uk1s3.embassy.ebi.ac.uk/bia-zarr-test/normal_26.ome.zarr/0", "https://uk1s3.embassy.ebi.ac.uk/bia-zarr-test/normal_26_mask.ome.zarr/0"),
+]
+
+
+# @pytest.mark.parametrize("bmz_model", models)
 @pytest.mark.parametrize("dataset", datasets)
-def test_model_prediction_on_bia(bmz_model,dataset):
+# def test_model_prediction_on_bia(bmz_model,dataset):
+def test_model_prediction_on_bia(dataset):
+    
     """
     Test the benchmark_bmz_on_bia_data.py main function using all model names, dataset URIs, and mask URI.
     """
     # Call the main function with the test input
+    bmz_model, input_uri, ref_uri = dataset
     output_array = process(
         bmz_model=bmz_model,
-        ome_zarr_uri=dataset,
+        ome_zarr_uri=input_uri,
+        reference_annotations=ref_uri,
         plot_images=False,  # Disable plot for testing
     )
 
     # Ensure the output is a numpy array
-    assert isinstance(output_array, np.ndarray), f"Output is not a numpy array for model {bmz_model}"
+    # assert isinstance(output_array, np.ndarray), f"Output is not a numpy array for model {bmz_model}"
 
-    # Ensure the output has content (non-empty array)
-    assert output_array.size > 0, f"Output array is empty for model {bmz_model}"
+    # # Ensure the output has content (non-empty array)
+    # assert output_array.size > 0, f"Output array is empty for model {bmz_model}"
 
-@pytest.mark.parametrize("tensor", tensors)
-@pytest.mark.parametrize("bmz_model", models)
-def test_model_prediction(tensor, bmz_model):
-    return run_model_inference(tensor, bmz_model)
+# @pytest.mark.parametrize("tensor", tensors)
+# @pytest.mark.parametrize("bmz_model", models)
+# def test_model_prediction(bmz_model,tensor):
+#     return run_model_inference(bmz_model,tensor)
