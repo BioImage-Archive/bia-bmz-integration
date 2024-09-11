7bc2471f3df45adc681169b9dd5e2427bd98d263
Craig Russell <ctr26@ebi.ac.uk>
2024-09-11 15:23:23 +0100

    formatting

--- bia_bmz_integration/tests/test_process.py
+++ bia_bmz_integration/tests/test_process.py
@@ -1,6 +1,6 @@
 import pytest
 import numpy as np
-from bia_bmz_integration import process,run_model_inference
+from bia_bmz_integration import process, run_model_inference
 
 # tensors = [
 #     np.random.rand(3,512,512),
@@ -10,23 +10,37 @@ from bia_bmz_integration import process,run_model_inference
 
 # ]
 
-models = ["loyal-squid", "noisy-fish", "affable-shark", "powerful-chipmunk", "loyal-parrot", "faithful-chicken", "frank-water-buffalo"]
+models = [
+    "loyal-squid",
+    "noisy-fish",
+    "affable-shark",
+    "powerful-chipmunk",
+    "loyal-parrot",
+    "faithful-chicken",
+    "frank-water-buffalo",
+]
 # datasets = ["S-BIAD1026", "S-BIAD634", "S-BIAD144/S-BIAD916", "BSST666"]
 
-datasets = {"S-BIAD144+S-BIAD916":
-("https://uk1s3.embassy.ebi.ac.uk/bia-zarr-test/23_low.ome.zarr/0","https://uk1s3.embassy.ebi.ac.uk/bia-zarr-test/23_high.ome.zarr/0"),
-
-"BIAD1026": ("https://uk1s3.embassy.ebi.ac.uk/bia-zarr-test/experimentA_01_WT_PDMP.ome.zarr/0", "https://uk1s3.embassy.ebi.ac.uk/bia-zarr-test/experimentA_01_WT_PDMP_cp_mask.ome.zarr/0" ),
-
-"S-BIAD634":("https://uk1s3.embassy.ebi.ac.uk/bia-zarr-test/normal_26.ome.zarr/0", "https://uk1s3.embassy.ebi.ac.uk/bia-zarr-test/normal_26_mask.ome.zarr/0"),
+datasets = {
+    "S-BIAD144+S-BIAD916": (
+        "https://uk1s3.embassy.ebi.ac.uk/bia-zarr-test/23_low.ome.zarr/0",
+        "https://uk1s3.embassy.ebi.ac.uk/bia-zarr-test/23_high.ome.zarr/0",
+    ),
+    "BIAD1026": (
+        "https://uk1s3.embassy.ebi.ac.uk/bia-zarr-test/experimentA_01_WT_PDMP.ome.zarr/0",
+        "https://uk1s3.embassy.ebi.ac.uk/bia-zarr-test/experimentA_01_WT_PDMP_cp_mask.ome.zarr/0",
+    ),
+    "S-BIAD634": (
+        "https://uk1s3.embassy.ebi.ac.uk/bia-zarr-test/normal_26.ome.zarr/0",
+        "https://uk1s3.embassy.ebi.ac.uk/bia-zarr-test/normal_26_mask.ome.zarr/0",
+    ),
 }
 
 
 @pytest.mark.parametrize("bmz_model", models)
 @pytest.mark.parametrize("dataset", datasets)
 # def test_model_prediction_on_bia(bmz_model,dataset):
-def test_model_prediction_on_bia(bmz_model,dataset):
-    
+def test_model_prediction_on_bia(bmz_model, dataset):
     """
     Test the benchmark_bmz_on_bia_data.py main function using all model names, dataset URIs, and mask URI.
     """
@@ -44,12 +58,16 @@ def test_model_prediction_on_bia(bmz_model,dataset):
 
     # # Ensure the output has content (non-empty array)
     # assert output_array.size > 0, f"Output array is empty for model {bmz_model}"
+
+
 import bioimageio.core
+
+
 @pytest.mark.parametrize("bmz_model", models)
 def test_model_prediction(bmz_model):
     model_resource = bioimageio.core.load_description(bmz_model)
     shape = model_resource.inputs[0].shape
     if type(shape) != list:
-        shape = [1,512,512]
+        shape = [1, 512, 512]
     arr = np.zeros(shape)
-    return run_model_inference(bmz_model,arr)
+    return run_model_inference(bmz_model, arr)
