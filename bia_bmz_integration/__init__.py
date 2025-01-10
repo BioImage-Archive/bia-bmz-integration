27c6fcb261b5bdf66729d85eeff6b7b7062a0daf
Chr1st0p43rR <190404576+Chr1st0p43rR@users.noreply.github.com>
2025-01-10 09:43:56 +0000

    Folder / file restructuring

--- bia_bmz_integration/__init__.py
+++
@@ -1,1 +0,0 @@
-from .run_benchmark_models import *
--- bia_bmz_integrator/scripts/run_benchmark_bmz_on_bia_data.py
+++ bia_bmz_integrator/scripts/run_benchmark_bmz_on_bia_data.py
@@ -1,10 +1,11 @@
 import click
-from bia_bmz_integration import process_run
-   
+from bia_bmz_integrator.run_benchmark_models import process_benchmark, process_run
+
 
 @click.command()
 @click.argument("bmz_model")
 @click.argument("ome_zarr_uri")
+@click.argument("reference_annotations", required=False)
 @click.option("-c", "--crop_image", nargs=2, type= int,
               help="crop the input image to obtain an image with the size specified. First value is x second is y]")
 @click.option("-z", "--z_slices", nargs=2, type= int,
@@ -18,9 +19,11 @@ from bia_bmz_integration import process_run
 @click.option("-b_ch", "--benchmark_channel", type= int, default=0,
               help="select a channel to benchmark from the prediction")
 
-def main(bmz_model,ome_zarr_uri,reference_annotations,plot_images,crop_image, z_slices,channel,t_slices, benchmark_channel):
-    return process_run(bmz_model,ome_zarr_uri, plot_images,crop_image, z_slices,channel,t_slices, benchmark_channel)
-
-
-if __name__ == "__main__":
-    main()
+def cli(bmz_model,ome_zarr_uri,reference_annotations,plot_images,crop_image, z_slices,channel,t_slices, benchmark_channel):
+   if reference_annotations:
+      scores = process_benchmark(bmz_model,ome_zarr_uri,reference_annotations,plot_images,crop_image, z_slices,channel,t_slices, benchmark_channel)  
+      metrics = dict(zip(['Precision','Recall','IoU', 'Dice', 'PSNR','NRMSE','SSIM'], scores))
+      print(metrics)
+      return metrics
+   else:
+      return process_run(bmz_model,ome_zarr_uri, plot_images,crop_image, z_slices,channel,t_slices, benchmark_channel)
